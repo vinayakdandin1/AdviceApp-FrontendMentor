5bc9789fa77db58910e4c93ab32062601f5f2d13
Vinayak Dandin <48254649+vinayakdandin1@users.noreply.github.com>
2022-04-26 15:09:02 +0200

    complete fetch advice from api and show on mobile layout without dice seperator

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios'
 
 function App() {
+
+  const [advice, setAdvice] = useState("")
+
+  const fetchNewAdvice = async () => {
+    const newAdvice = await axios.get("https://api.adviceslip.com/advice")
+    console.log(newAdvice.data);
+    setAdvice(newAdvice.data.slip)
+  }
+
+  useEffect(() => {
+    fetchNewAdvice()
+  }, [])
+
   return (
     <div className="App">
-      Hello
+      <div className='advice-container' >
+        <h5>advice # {advice.id}</h5>
+        <p>{advice.advice}</p>
+      </div>
+      
     </div>
   );
 }
